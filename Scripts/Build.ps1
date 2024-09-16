17fe91ce363521c74a5751ed86da065b4118537b
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-09-16 20:40:35 +0800

    fix CI fix Cmake fix AutoBuild Script

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -2,20 +2,19 @@ $VerbosePreference = 'SilentlyContinue'
 
 Set-Location ..
 
-$CMAKE_ARGS = ""
+#Remove-Item -Recurse -Force ./Build
 
-Get-Content "./Scripts/Common.txt" | ForEach-Object {
-    $CMAKE_ARGS += " $_"
-}
+#$CMAKE_ARGS = ""
+#Get-Content "./Scripts/Common.txt" | ForEach-Object {
+#    $CMAKE_ARGS += "$_ "
+#}
+#Write-Host "CMAKE_ARGS: $CMAKE_ARGS"
 
-cmake -G "Ninja" -B Build $CMAKE_ARGS
+cmake -G "Ninja" -B Build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_BINARY_DIR=.
+
 
 cmake --build Build
 
-Set-Location Build
-
-if (-Not (Test-Path -Path "compiled")) {
-    New-Item -ItemType Directory -Path "compiled"
-}
+Set-Location Build/compiled
 
 
